55b072f69dbc6f1af7f857c9906466eba186e03f
dylant44 <dt303271@students.davenport.k12.ia.us>
2019-04-18 11:19:22 -0500

    Main and convert

--- src/Convert.java
+++ src/Convert.java
@@ -1,4 +1,15 @@
 public class Convert
 {
+    public static String conversion(String word)
+    {
+        String convertedWord  = null;
+        String tempString = null;
+        String firstLetter = word.substring(0,0);
+        tempString = word.substring(0, 0) + word.substring(0, word.length() - 1);
+        convertedWord = tempString + firstLetter + "ay";
 
+
+
+        return convertedWord;
+    }
 }
